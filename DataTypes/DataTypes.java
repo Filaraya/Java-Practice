2fd58b3f5851e5b98c646e3ce62b66a22e5c1291
filaraya <filijosi@gmail.com>
2021-09-28 16:14:57 -0700

    updated

---
+++ DataTypes/DataTypes.java
@@ -0,0 +1,25 @@
+
+/**
+ * The most frequently data type in java are.
+ * char,String,int,float and boolean.
+ * @author Filmon 
+ * @version 09-28-2021
+ */
+public class DataTypes {
+    
+    public static void main (String [] args){
+        char letter ='M';
+        String title = "love Java";
+        int num = 345;
+        float measure = 4.785f;
+        boolean result = true;
+        
+        System.out.println (letter + " is the first letter.");
+        System.out.println ("The title of the book is " + title +".");
+        System.out.println ("The house number is " + num + ".");
+        System.out.println ("The length of the metal is " + measure +".");
+        System.out.println (result + " was the output.");
+        
+    }
+    
+}
