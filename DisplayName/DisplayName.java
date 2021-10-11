3ef70a0bb29d8dbe3a48ca7339ee3fbd60bed000
filaraya <filijosi@gmail.com>
2021-10-10 22:30:16 -0700

    file added

---
+++ DisplayName/DisplayName.java
@@ -0,0 +1,7 @@
+public class DisplayName {
+    public static void main (String[] args) {
+        Name myname = new Name(); //creating a new object of Name class
+        myname.display(); //executing the display () method in the Name class
+    }
+    
+}
