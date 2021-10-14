8a5e61bf410304e96536b3a4417f5fc95079142d
filaraya <filijosi@gmail.com>
2021-10-13 20:16:26 -0700

    file added

---
+++ ForLoop/ForLoop.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of ForLoop here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ForLoop {
+    public static void main (String [] args)
+    {
+     int num = 0;
+     for (int i = 0; i<4;i++)
+     {
+         System.out.println("number" + i);
+         
+        }
+        
+    }
+
+}
---
+++ MyAccount/Main.java
@@ -0,0 +1,27 @@
+
+/**
+ * Write a description of MyAccount here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+//import static java.lang.System.out;
+public class Main {
+  static void myMethod() {
+    System.out.println("I just got executed!");
+  }
+
+  public static void main(String[] args) {
+    myMethod();
+    myMethod();
+    myMethod();
+  }
+}
+
+// I just got executed!
+// I just got executed!
+// I just got executed!
+ 
+ 
+ 
+
